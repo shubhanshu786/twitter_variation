8def443758959b0673b88063e12f318060890acc
Shubhanshu Sharma <dec31.shubh@gmail.com>
2018-09-01 23:20:16 +0800

    python 2 encoding issue fix

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 import pickle
 import os
+import sys
 
 name="twitter_variation"
-version="1.0"
+version="1.0.1"
 author="Shubhanshu Sharma"
 
 english_variation_file_path = os.path.join(os.path.dirname(__file__),'variation_to_english_dict.pickle')
@@ -12,4 +13,6 @@ with open(english_variation_file_path,'rb') as f:
 def variation_to_english(sent=''):
     for k,v in variation_dict.items():
         sent = sent.replace(k,v)
+    if sys.version_info[0]<3:
+        return sent.encode()
     return sent
